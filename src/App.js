d8b40288f58265fbd812cd9fbc5a12298302f601
ajaymuttamsettycloud <ajaymuttamsettycloud@gmail.com>
2021-10-28 22:00:40 -0400

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       <p>
+         This is cloud computing assignment based on a small web page
+         
+         <p>by</p>
+         <h2>Ajay Muttamsetty</h2>
+         </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
